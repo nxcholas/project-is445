f35948bae410e10fc90323468d5005d3d4edccb9
Nicholas Nguyen <nxcholas4652@gmail.com>
2025-05-09 09:19:18 -0700

    update web config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), viteStaticCopy({
+      targets: [
+        {
+          src: 'web.config',
+          dest: '' // puts it at the root of dist/
+        }
+      ]
+    })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
